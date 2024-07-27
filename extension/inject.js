8832ebcd40941a93db3378d10e28c28cb1ac027a
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2024-07-27 23:12:03 +0530

    cleanup

--- extension/inject.js
+++ extension/inject.js
@@ -1,4 +1,6 @@
-let reportButton;
+const reportButton = document.createElement("button");
+reportButton.classList.add("clickbait-btn");
+reportButton.textContent = "Report";
 
 async function checkClickbait(videoId) {
   return true;
@@ -8,68 +10,71 @@ async function reportVideo(videoId) {
   return true;
 }
 
-function newTagElement() {
-  const elm = document.createElement("span");
-  elm.classList.add("clickbait");
-  elm.textContent = "Clickbait";
-  return elm;
-}
-
-async function handleAddedNode(node) {
-  if (!node.tagName) return;
-  if (
-    node.tagName.toLowerCase() == "a" &&
-    (node.href.includes("/watch?v=") || node.href.includes("/shorts/")) &&
-    node.classList.contains("ytd-thumbnail")
-  ) {
-    // detects all newly added video thumbnail elements
-    const videoId = new URL(node.href).searchParams.get("v");
-    const isClickbait = await checkClickbait(videoId);
-    if (isClickbait) node.parentNode.appendChild(newTagElement());
-  } else if (
-    window.location.pathname == "/watch" &&
-    node.id.toLowerCase() == "movie_player"
-  ) {
-    // detects the watch page and youtube player container in which button is to be inserted
-    document.getElementById("movie_player").appendChild(reportButton);
-  }
-}
-
-const observer = new MutationObserver((mutationsList) => {
-  for (const mutation of mutationsList) {
-    if (mutation.type === "childList") {
-      Array.from(mutation.addedNodes).forEach((node) => handleAddedNode(node));
-    }
+reportButton.addEventListener("click", async () => {
+  const videoId = new URL(window.location.href).searchParams.get("v");
+  if (confirm("Report this video as spam or misleading.")) {
+    await reportVideo(videoId);
   }
 });
 
-function waitForContentLoad() {
+function newTagElement() {
+  const element = document.createElement("span");
+  element.classList.add("clickbait");
+  element.textContent = "Clickbait";
+  return element;
+}
+
+function getVideoId(href) {
+  const url = new URL(href);
+  if (url.pathname.includes("/shorts")) {
+    return url.pathname.slice(8);
+  } else return url.searchParams.get("v");
+}
+
+const thumbnailObserver = new MutationObserver((mutationsList) => {
+  for (const mutation of mutationsList) {
+    Array.from(mutation.addedNodes).forEach(async (node) => {
+      if (!node.tagName) return;
+      if (
+        node.tagName.toLowerCase() == "a" &&
+        node.id == "thumbnail" &&
+        node.classList.contains("inline-block")
+      ) {
+        // here gets all newly added video thumbnail elements
+        const videoId = getVideoId(node.href);
+        const isClickbait = await checkClickbait(videoId);
+        if (isClickbait) node.parentNode.appendChild(newTagElement());
+      }
+    });
+  }
+});
+
+function waitForElementLoad(type, data) {
   return new Promise((resolve) => {
     const intervalId = setInterval(() => {
-      const targetNode = document.getElementById("content");
-      if (targetNode) {
+      let element;
+      if (type == "tag") element = document.getElementsByTagName(data)[0];
+      else element = document.getElementById(data);
+      if (element) {
         clearInterval(intervalId);
-        resolve(targetNode);
+        resolve(element);
       }
     }, 200);
   });
 }
 
-async function handleReportClick() {
-  if (confirm("Report this video as spam or misleading.")) {
-    await reportVideo(new URL(window.location.href).searchParams.get("v"));
+const routeObserver = new MutationObserver(async () => {
+  if (window.location.pathname == "/watch") {
+    const playerElement = await waitForElementLoad("id", "movie_player");
+    playerElement.appendChild(reportButton);
   }
-}
+});
 
 async function init() {
-  const targetNode = await waitForContentLoad();
-
-  reportButton = document.createElement("button");
-  reportButton.classList.add("clickbait-btn");
-  reportButton.textContent = "Report";
-  reportButton.addEventListener("click", handleReportClick);
-
-  observer.observe(targetNode, { childList: true, subtree: true });
+  const contentNode = await waitForElementLoad("id", "content");
+  thumbnailObserver.observe(contentNode, { childList: true, subtree: true });
+  const titleNode = await waitForElementLoad("tag", "title");
+  routeObserver.observe(titleNode, { childList: true });
 }
 
 init();
